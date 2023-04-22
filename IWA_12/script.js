01ce86cea404aff1ae23f39e4b91ed88a1cf9a38
Nwabisa <nwabisa.mawu@gmail.com>
2023-04-22 16:30:05 +0200

    added for loop to make changes according to STATUS_MAP object

--- IWA_12/script.js
+++ IWA_12/script.js
@@ -27,41 +27,62 @@ const STATUS_MAP = {
 
 // Edit below line 
 
-//MAKE BUTTONS GRAYSCALE/BLACK - NO COLOR
+//MAKE books GRAYSCALE/BLACK - NO COLOR
 
 /* get the button elements to be changed*/
-const allButtons = document.querySelectorAll("button");
-/* use the style object to change text-color for the three incorrect buttons*/
-allButtons[2].style.color = "";
-allButtons[5].style.color = "";
-allButtons[8].style.color = "";
+const allButtons = document.getElementsByTagName("button");
+/* use the style object to change text-color for the three incorrect books*/
+for (let i = 0; i < allButtons.length; i++) {
+    allButtons[i].style.color = ""
+}
 
 
 //CHANGE STATUS TEXT TO CORRESPOND TO OBJECT.
 
 /* fetch the element to be chnaged*/
-const fetchStatus = document.getElementsByTagName("span");
+const fetchStatus = document.getElementsByClassName("status");
+
+const books = document.getElementsByTagName("div");
 
 /* use indexes to change the text colors using the style object 
 I used the set.attribute method instead of style object property 
-because I want to replace the style, and not just change it*/
-fetchStatus[0].setAttribute("style", "color: red");
-fetchStatus[1].setAttribute("style", "color: blue");
-fetchStatus[2].setAttribute("style", "color: green");
+because I want to replace the style, and not just change it
+
+I used a for loop that will iterate over all the book divs in the page
+and then check the status of the span element then adjust the buttons
+inside that book div accordingly */
 
 
-//CHANGE BUTTONS ENABLE/DISABLE TO MATCH OBJECT
-//buttons for book 1//
-allButtons[0].disabled = true;
-allButtons[1].disabled = true;
-allButtons[2].disabled = false;
-//buttons for book 2//
-allButtons[3].disabled = true;
-allButtons[4].disabled = false;
-allButtons[5].disabled = true;
-//buttons for book 3//
-allButtons[6].disabled = false;
-allButtons[7].disabled = false;
-allButtons[8].disabled = true;
+for (let i = 0; i < books.length; i++) {
+    const reserveBtn = books[i].getElementsByClassName("reserve")[0]
+    const checkoutBtn = books[i].getElementsByClassName("checkout")[0]
+    const checkinBtn = books[i].getElementsByClassName("checkin")[0]
+
+    if (fetchStatus[i].innerHTML === 'shelf') {
+        fetchStatus[i].setAttribute("style", "color: green");
+        checkinBtn.disabled = true;
+    }
+    if (fetchStatus[i].innerHTML === 'reserved') {
+        fetchStatus[i].setAttribute("style", "color: blue");
+        reserveBtn.disabled = true;
+        checkinBtn.disabled = true;
+    }
+    if (fetchStatus[i].innerHTML === 'overdue') {
+        fetchStatus[i].setAttribute("style", "color: red");
+        reserveBtn.disabled = true;
+        checkoutBtn.disabled = true;
+    }
+    if (fetchStatus[i].innerHTML === 'checkedOut') {
+        fetchStatus[i].setAttribute("style", "color: orange");
+        reserveBtn.disabled = true;
+        checkoutBtn.disabled = true;
+    }
+
+};
+
+
+
+
+
 
 
