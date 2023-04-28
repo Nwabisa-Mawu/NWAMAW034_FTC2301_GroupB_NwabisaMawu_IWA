a5950d71b3181f783fd6d36ee3f0bfc5dbec5cb7
Nwabisa <nwabisa.mawu@gmail.com>
2023-04-29 01:00:27 +0200

    IWA_17 complete, IWA_18 adding files to repo

--- IWA_17/script.js
+++ IWA_17/script.js
@@ -18,8 +18,6 @@ const getDaysInMonth = (date) => new Date(date.getFullYear(), date.getMonth() + 
 
 // Only edit below 
 
-
-
 /**
  * Function that creates an empty array of the given length parameter
  * to be filled later.
@@ -38,20 +36,12 @@ const createArray = (length) => {
 const createData = () => {
 
     let weeks = createArray(5)
-    let days = createArray(7)
 
     /* for loop to create the text inside the week column cells
     and add them to the weeks array and the number of rows for
     the weeks column */
     for (const [index, singleWeek] of weeks.entries()) {
         weeks[index] = `Week ${index + 1}`
-    };
-
-    /* for of loop to create the text inside the days cells with
-    the number of days columns for each row*/
-    for (const [index, singleDay] of days.entries()) {
-
-        days[index] = index + 1;
     };
 
     return weeks;
@@ -73,65 +63,59 @@ const addCell = () => {
 };
 
 
-// const createHtml = (data) => {
-//fetch the section of the table to add the information to
-const tableBody = document.querySelector('[data-content]');
+const createHtml = (data) => {
 
 /* create for loop to use the addCell function 5 times
 to make 5 rows - started index from one for readability*/
 for (let i = 1; i < 6; i++) {
-    tableBody.appendChild(addCell())
+    document.body.appendChild(addCell())
 }
 
-//fetch all the first cells of each row
-const rows = tableBody.querySelectorAll('tr');
-
 //for loop to add the week text to the first cell of each row
-
+const rows = document.body.querySelectorAll('tr');
 const currentDay = new Date()
 const daysInMonth = getDaysInMonth(currentDay)
 
-for (let i = 0; i < 5; i++) {
-  let weekCells = rows[i].querySelectorAll('td')
-   weekCells[0].classList.add('table__cell_sidebar')
-   weekCells[0].innerHTML = createData()[i];
+for (let i = 1; i < 6; i++) {
+//fetch all the first cells of each row
 
+  const weekCells = rows[i].querySelectorAll('td')
+   weekCells[0].classList.add('table__cell_sidebar')
+   weekCells[0].innerHTML = data[i - 1];
 
    for (let x = 1; x < 8; x++) {
     let day = x+1
     weekCells[x].innerHTML = day
 
     //used to add the light color to the weekends
-    if (x === 1 || x === 7) {
+    if ((x === 1 || x === 7) ) {
         weekCells[x].classList.add('table__cell_weekend')
     }
-
+    
     /* to populate the day cells with the month days */
-    if(i === 1 && day < daysInMonth) {
+    if(i === 2 && day < daysInMonth) {
         day +=7
         weekCells[x].innerHTML = day
         weekCells[x].classList.add('table__cell_alternate')
-    } else if (i === 2 && day < daysInMonth) {
+    } else if (i === 3 && day < daysInMonth) {
         day +=14
         weekCells[x].innerHTML = day
-    } else if (i === 3 && day < daysInMonth) {
+    } else if (i === 4 && day < daysInMonth) {
         day +=21
         weekCells[x].innerHTML = day
         weekCells[x].classList.add('table__cell_alternate')
-    } else if (i === 4 && day < daysInMonth) {
+    } else if (i === 5 && day < daysInMonth) {
         day +=28
         weekCells[x].innerHTML = day
         if (day === daysInMonth) break;
     }
-
-    if (day === currentDay.getDate()) {
-        weekCells[x].classList.add('table__cell_today')
-    }
+   //used to mark the current day
+   if (day === currentDay.getDate()) {
+    weekCells[x].classList.add('table__cell_today')
+}
    }
 }
-// };
-
-
+};
 
 
 // Only edit above
@@ -139,6 +123,5 @@ for (let i = 0; i < 5; i++) {
 const current = new Date()
 document.querySelector('[data-title]').innerText = `${MONTHS[current.getMonth()]} ${current.getFullYear()}`
 
-// const data = createData()
-// console.log(data)
-// document.querySelector('[data-content]').innerHTML = createHtml(data);
+const data = createData()
+document.querySelector('[data-content]').innerHTML = createHtml(data);
